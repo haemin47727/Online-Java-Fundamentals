966c6ef40d5e2b4c69edd348ea0f311b50ab2e37
Julia Song <sginstockx@gmail.com>
2025-06-21 18:34:30 -0400

    Exercise 5

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -13,13 +13,17 @@ public class Exercise_05 {
     public static void main(String[] args) {
 
         String str = "hello!";
+        String str2 = "hello";
         // please declare an int variable below, and set it to the value of the length of "str"
         int strLength = str.length();
-        String str2 = "hello";
+        System.out.println("The length of the string is " + strLength);
+
         // please initialize a boolean variable and test whether str is equal to str2
         boolean equalStrings = str.equals(str2);
+        System.out.println("Is string 1 equal to string 2? " + equalStrings);
         // please concatenate str & str2 and set the result to a new String variable below
-
+        String str3 = str2.concat(" ").concat(str);
+        System.out.println(str3);
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
 
