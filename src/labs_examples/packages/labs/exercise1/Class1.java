0be0fb2e806d1c5ee26c0d08692f7a2a8f1d73f3
haemin47727 <sginstockx@gmail.com>
2025-07-22 21:14:26 -0400

    week 5 package exercise

---
+++ src/labs_examples/packages/labs/exercise1/Class1.java
@@ -0,0 +1,11 @@
+package labs_examples.packages.labs.exercise1;
+
+public class Class1 {
+    protected static void class1(){
+        System.out.println("protected class 1");
+    }
+    public static void main(String[] args){
+        //Class2.class2();
+        class1();
+    }
+}
---
+++ src/labs_examples/packages/labs/exercise2/Class2.java
@@ -0,0 +1,11 @@
+package labs_examples.packages.labs.exercise2;
+
+public class Class2 {
+    protected static void class2(){
+        System.out.println("protected class 2");
+    }
+    public static void main(String[] args){
+        //Class1.class1();
+        class2();
+    }
+}
