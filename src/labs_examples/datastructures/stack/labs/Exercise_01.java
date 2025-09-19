4e81886a30c580b8bc4929542dc7248392aca52b
Julia Song <sginstockx@gmail.com>
2025-09-19 16:10:18 -0400

    lab

--- src/labs_examples/datastructures/stack/labs/Exercise_01.java
+++ src/labs_examples/datastructures/stack/labs/Exercise_01.java
@@ -6,3 +6,39 @@ package labs_examples.datastructures.stack.labs;
  *      1) instantiate a new Stack
  *      2) demonstrate the use of EVERY method in the Stack class - there are 5
  */
+import java.util.Stack;
+
+public class Exercise_01 {
+    public static void main(String[] args) {
+
+        Stack<String> stack = new Stack<>();
+
+
+        stack.push("apple");
+        stack.push("banana");
+        stack.push("cherry");
+        System.out.println("Stack after pushes: " + stack);
+
+
+        String top = stack.peek();
+        System.out.println("Top item : " + top);
+        System.out.println("Stack after peek : " + stack);
+
+
+        String popped = stack.pop();
+        System.out.println("Popped item: " + popped);
+        System.out.println("Stack after pop: " + stack);
+
+
+        boolean isEmpty = stack.empty();
+        System.out.println("Is the stack empty? " + isEmpty);
+
+
+        int position = stack.search("apple");
+        System.out.println("Position of apple from the top: " + position);
+
+
+        int notFound = stack.search("durian");
+        System.out.println("Position of durian from the top: " + notFound);
+    }
+}
